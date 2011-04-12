3a291cc75e65d9a1cdf1a16f2f9662160377ccaa
John McKerrell <john@mckerrell.net>
2011-04-12 18:52:59 +0100

    Forgot to output pin from control block.

--- app/models/control_block.rb
+++ app/models/control_block.rb
@@ -3,7 +3,9 @@ class ControlBlock < ActiveRecord::Base
   belongs_to :digital_output
 
   def to_api
-    "C:#{num_or_star(minute)} #{num_or_star(hour)} #{num_or_star(day)} #{num_or_star(month)} #{num_or_star(weekday)} #{len}#{state ? '' : ' 0'}"
+    pin = self.digital_output.address
+    pin = pin.slice(1,pin.length-1)
+    "C:#{num_or_star(minute)} #{num_or_star(hour)} #{num_or_star(day)} #{num_or_star(month)} #{num_or_star(weekday)} #{len} #{pin}#{state ? '' : ' 0'}"
   end
 
   private
