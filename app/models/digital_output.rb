4218e19b6deb84045695aacaf5bac322f0e82240
John McKerrell <john@mckerrell.net>
2011-04-14 14:02:55 +0000

    Fixing references for digital outputs.

--- app/models/digital_output.rb
+++ app/models/digital_output.rb
@@ -1,5 +1,5 @@
 class DigitalOutput < ActiveRecord::Base
-  belongs_to :type, :class_name => 'AnalogInputType', :foreign_key => 'type_id'
+  belongs_to :type, :class_name => 'DigitalOutputType', :foreign_key => 'type_id'
   belongs_to :base_station
   has_many :control_blocks
   belongs_to :transient_control_block, :class_name => 'ControlBlock', :foreign_key => 'transient_control_block_id'
