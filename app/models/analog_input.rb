50ca5cbe6a70b9639641e1be6ec6e6ffef8a366f
John McKerrell <john@mckerrell.net>
2011-05-18 21:19:23 +0100

    Adding the temperature analog input widget

--- app/models/analog_input.rb
+++ app/models/analog_input.rb
@@ -3,6 +3,23 @@ class AnalogInput < ActiveRecord::Base
   belongs_to :base_station
   has_many :analog_input_logs
 
+  def recent_logs( limit )
+    AnalogInputLog.find(:all,
+      :limit => limit,
+      :order => "created_at DESC",
+      :conditions => [ "analog_input_id = ?", self.id ] )
+  end
+
+  def recent_logs_humanized( limit )
+    analog_input_logs = self.recent_logs(limit)
+
+    logs = []
+    analog_input_logs.each do |l|
+      logs << { :created_at => l.created_at, :value => l.value, :human_value => self.humanize_value(l.value) }
+    end
+    logs
+  end
+
   def humanize_value(value)
     case self.type.code
     when "temperature"
