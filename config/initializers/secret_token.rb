f45c3a5a49a7a5b8e1627fb568b385d8fe00296f
John McKerrell <john@mckerrell.net>
2013-04-05 11:02:33 +0100

    Changes made by `rails new .` (with some revisions)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YahmsNetDevelopment::Application.config.secret_token = '61ee14dfce46d121360de5b0f1b4c5681d00a3737351bd44bbbf2399c0b219b1aad6e2194437bf0426fc346de09a22191229d55692430ed416faf8bee20d68d9'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_yahms_net_session',
-  :secret      => 'e36de45a97d6e3e02976957c26e2bf384403b7aa6cc8b88394dddfd1c659a64730cf6951229628c3b2fc9992eb7946befda78fb0b711d994b5d9a3ad5897601b'
-}
+YahmsNetDevelopment::Application.config.session_store :cookie_store, key: '_yahms_net_development_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# YahmsNetDevelopment::Application.config.session_store :active_record_store
